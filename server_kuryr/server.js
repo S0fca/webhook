b54a77b49726d05ef89a5e3b74881b4db26ff335
Sam <saminkoh@gmail.com>
2025-11-12 19:08:46 +0100

    notifikace, X_Signature

--- server_kuryr/server.js
+++ server_kuryr/server.js
@@ -1,13 +1,60 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+const crypto = require('crypto');
 
+
+const SHARED_SECRET = 'tajneheslo123';
 // Endpoint pro přijetí nové objednávky
+function sendWebhook(order, status) {
+    const body = JSON.stringify({
+        id: order.id,
+        status,
+        event_id: crypto.randomUUID(), // unikátní ID události
+    });
+
+    const sig = crypto.createHmac('sha256', SHARED_SECRET)
+        .update(body, 'utf8')
+        .digest('hex');
+
+    fetch(order.callbackUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Signature': sig,
+        },
+        body,
+    })
+        .then(res => console.log(`Webhook ${order.id} (${status}) → ${res.status}`))
+        .catch(err => console.error('Webhook error:', err));
+}
+
 app.post('/order', (req, res) => {
-const order = req.body;
-console.log('Přijata objednávka:', order);
-res.sendStatus(202);  // odpověď 202 Accepted – objednávka převzata k dalšímu zpracování
+    const order = req.body;
+    console.log(`Přijata objednávka: ${order.id}`);
+    res.sendStatus(202);
+
+    setTimeout(() => {
+        console.log(`${order.id}: Restaurace připravuje`);
+        sendWebhook(order, 'Restaurace připravuje');
+
+
+        setTimeout(() => {
+            console.log(`${order.id}: Rozváží se`);
+            sendWebhook(order, 'Rozváží se');
+
+            setTimeout(() => {
+                console.log(`${order.id}: Doručeno`);
+                sendWebhook(order, 'Doručeno');
+            }, 5000);
+
+        }, 5000);
+    }, 5000);
 });
+
+
+//res.sendStatus(202);  // odpověď 202 Accepted – objednávka převzata k dalšímu zpracování
+
 
 app.listen(3000, () => {
     console.log('Courier runs on http://localhost:3000');
